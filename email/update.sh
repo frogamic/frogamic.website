369c76eb7353cf8e524be80c38763fb2917c36ad
Dominic Shelton <frogamic@protonmail.com>
2020-08-09 21:01:16 +1000

    Refactor email stack, add phashion.moe

---
+++ email/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+cd "$(dirname "$0")"
+
+ACTION="update"
+DOMAIN=$1
+if [[ $# -eq "2" ]]; then
+	ACTION="${2}"
+fi
+
+aws cloudformation "${ACTION}-stack" --stack-name "protonmail-${DOMAIN/./-}" --template-body "file://$(pwd)/protonmail.yml" --parameters "file://$(pwd)/${DOMAIN}.json" --region ap-southeast-2
