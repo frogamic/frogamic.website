4a01b35f3a6b679956467d32d5e3878ac36b3bd1
Dominic Shelton <frogamic@protonmail.com>
2020-10-18 12:36:12 +1100

    Fix cert hostedzoneid, set region

--- static-site/deploy.sh
+++ static-site/deploy.sh
@@ -7,13 +7,17 @@ DOMAIN_NAME="frogamic.website"
 DOMAIN_NAME2="frogamic.com"
 PROJECT="frogamic-website"
 STAGE="prod"
+REGION="ap-southeast-2"
 
 cd "$(dirname "$0")"
 
 if [ $# -gt 0 ] && [[ " $@ " =~ " with-state " ]]; then
   echo "Deploying state buckets for ${DOMAIN_NAME}"
 
-  aws cloudformation deploy --stack-name "${PROJECT}-state" --template-file ./state.yml \
+  aws cloudformation deploy \
+    --stack-name "${PROJECT}-state" \
+    --template-file ./state.yml \
+    --region "$REGION" \
     --parameter-overrides \
       "Stage=${STAGE}" \
       "Project=${PROJECT}"
@@ -25,23 +29,35 @@ if [ $# -gt 0 ] && [[ " $@ " =~ " with-cert " ]]; then
   HOSTED_ZONE_ID="$(aws route53 list-hosted-zones-by-name --dns-name "$DOMAIN_NAME" --query 'HostedZones[0].Id' --output text)"
   HOSTED_ZONE_ID2="$(aws route53 list-hosted-zones-by-name --dns-name "$DOMAIN_NAME2" --query 'HostedZones[0].Id' --output text)"
 
+  # Remove leading `/hostedzone/`
+  HOSTED_ZONE_ID="${HOSTED_ZONE_ID##*/}"
+  HOSTED_ZONE_ID2="${HOSTED_ZONE_ID2##*/}"
+
   aws cloudformation deploy --stack-name "${PROJECT}-cert" --template-file ./cert.yml \
+    --region us-east-1 \
     --parameter-overrides \
       "Stage=${STAGE}" \
       "Project=${PROJECT}" \
       "DomainName=${DOMAIN_NAME}" \
       "HostedZoneId=${HOSTED_ZONE_ID}" \
       "DomainName2=${DOMAIN_NAME2}" \
-      "HostedZoneId2=${HOSTED_ZONE_ID2}" \
-    --region us-east-1
+      "HostedZoneId2=${HOSTED_ZONE_ID2}"
 
   CERT_ARN="$(aws cloudformation describe-stacks --region us-east-1 --query 'Stacks[0].Outputs[?OutputKey==`CertArn`].OutputValue' --output text --stack-name "${PROJECT}-cert")"
 
-  aws ssm put-parameter --overwrite --type "String" --name "/infra/${STAGE}/${PROJECT}/Cert" --value "${CERT_ARN}"
+  aws ssm put-parameter \
+    --region "$REGION" \
+    --name "/infra/${STAGE}/${PROJECT}/Cert" \
+    --value "${CERT_ARN}" \
+    --type "String" \
+    --overwrite
 fi
 
 echo "Deploying CloudFront stack for ${DOMAIN_NAME}"
-aws cloudformation deploy --stack-name "$PROJECT" --template-file ./distribution.yml \
+aws cloudformation deploy \
+  --region "$REGION" \
+  --stack-name "$PROJECT" \
+  --template-file ./distribution.yml \
   --parameter-overrides \
     "Stage=${STAGE}" \
     "Project=${PROJECT}" \
