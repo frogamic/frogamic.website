796d04cb3010f12dbafbdf301bfa43f5882e39d5
Dominic Shelton <frogamic@protonmail.com>
2023-04-13 12:23:06 +1000

    Use cloudformation deploy action for email stack

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ aws cloudformation deploy \
   --template-file ./hostedzones.yml \
   --region ap-southeast-2
 
-./email/update.sh frogamic.com create
-./email/update.sh frogamic.website create
+./email/update.sh frogamic.com
+./email/update.sh frogamic.website
 
 ./static-site/deploy.sh with-cert with-state
--- email/update.sh
+++ email/update.sh
@@ -6,14 +6,10 @@ set -o nounset
 
 cd "$(dirname "$0")"
 
-ACTION="update"
 DOMAIN=$1
-if [[ $# -eq "2" ]]; then
-	ACTION="${2}"
-fi
 
-aws cloudformation "${ACTION}-stack"  \
-	--stack-name "${DOMAIN/./-}-protonmail"  \
-	--template-body "file://$(pwd)/protonmail.yml"  \
-	--parameters "file://$(pwd)/${DOMAIN}.json"  \
+aws cloudformation deploy \
+	--stack-name "${DOMAIN/./-}-protonmail" \
+	--template protonmail.yml \
+	--parameter-overrides "file://$(pwd)/${DOMAIN}.json" \
 	--region ap-southeast-2
